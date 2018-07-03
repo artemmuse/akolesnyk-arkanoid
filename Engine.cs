c55b37b961d8ec73fc952d2ce7dd07524c125fd5
artemmuse <artemuse.it@gmail.com>
2018-07-03 22:17:05 +0300

    Creating code sources.

---
+++ Engine.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Engine
+    {
+        private int[,] consoleMatrix;
+
+        private void printConsoleMatrix()
+        {
+            for (int i = 0; i < consoleMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < consoleMatrix.GetLength(1); j++)
+                {
+                    if (consoleMatrix[i, j] == 1)
+                        Console.Write('*');
+                    else
+                        Console.Write(' ');
+                }
+                //Console.WriteLine();
+            }
+        }
+
+        public void Start()
+        {
+            consoleMatrix = new int[24, 80];
+
+            Square s = new Square(0,0);
+            Triangle t = new Triangle(2,10);
+            Circle c = new Circle(10,4);
+            s.isMoved = true;
+
+            BaseFigure[] figureArray = new BaseFigure[3] { s, t, c };
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.Escape)
+                    break;
+
+                Array.Clear(consoleMatrix, 0, consoleMatrix.Length);
+                Console.Clear();
+
+                foreach (BaseFigure figure in figureArray)
+                {
+                    if (figure.isMoved)
+                    {
+                        switch (key.Key)
+                        {
+                            case ConsoleKey.UpArrow:
+                                if (figure.X > 0)
+                                {
+                                    figure.X--;
+                                }
+                                break;
+                            case ConsoleKey.DownArrow:
+                                if (figure.X < 19)
+                                {
+                                    figure.X++;
+                                }
+                                break;
+                            case ConsoleKey.LeftArrow:
+                                if (figure.Y > 0)
+                                {
+                                    figure.Y--;
+                                }
+                                break;
+                            case ConsoleKey.RightArrow:
+                                if (figure.Y < 75)
+                                {
+                                    figure.Y++;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    figure.Draw(ref consoleMatrix);
+                }
+
+                printConsoleMatrix();
+            }
+        }
+    }
+}
---
+++ Figure.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    abstract class BaseFigure
+    {
+        public int X,Y;
+        public Boolean isMoved;
+        protected int[,] figureMatrix;
+
+        public BaseFigure(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public void Draw(ref int[,] consoleMatrix)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if(consoleMatrix[X + i, Y + j] == 0)
+                        consoleMatrix[X + i, Y + j] = figureMatrix[i, j];
+                }
+            }
+        }
+    }
+
+    class Square : BaseFigure
+    {
+        public Square(int X, int Y) : base(X,Y)
+        {  
+            figureMatrix = new int[5, 5] {  { 1, 1, 1, 1, 1 }, 
+                                            { 1, 0, 0, 0, 1 },
+                                            { 1, 0, 0, 0, 1 },
+                                            { 1, 0, 0, 0, 1 },
+                                            { 1, 1, 1, 1, 1 } };
+        }
+    }
+
+    class Triangle : BaseFigure
+    {
+        public Triangle(int X, int Y) : base(X, Y)
+        {
+            figureMatrix = new int[5, 5] {  { 0, 0, 1, 0, 0 },
+                                            { 0, 1, 0, 1, 0 },
+                                            { 0, 1, 0, 1, 0 },
+                                            { 1, 0, 0, 0, 1 },
+                                            { 1, 1, 1, 1, 1 } };
+        }
+    }
+
+    class Circle : BaseFigure
+    {
+        public Circle(int X, int Y) : base(X, Y)
+        {
+            figureMatrix = new int[5, 5] {  { 0, 0, 1, 0, 0 },
+                                            { 0, 1, 0, 1, 0 },
+                                            { 1, 0, 0, 0, 1 },
+                                            { 0, 1, 0, 1, 0 },
+                                            { 0, 0, 1, 0, 0 } };
+        }
+    }
+}
